f37dbc9e570e9009d4a4eefc1420e72f73ba9b38
johannesMayerl <may17007@spengergasse.at>
2016-12-06 16:23:31 +0100

    Testing

--- TestingAPI/Program.cs
+++ TestingAPI/Program.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
+using WienerLinienApi;
+using WienerLinienApi.Information;
 
 namespace TestingAPI
 {
@@ -10,8 +13,24 @@ namespace TestingAPI
     {
         static void Main(string[] args)
         {
+            test();
+        }
+
+        public static async void test()
+        {
             Console.WriteLine("Start");
-            var wlContext = new WienerLinienContext("yourApiKey");
+            var wlContext = new WienerLinienContext("O56IE8eH7Kf5R5aQ");
+            Console.WriteLine(wlContext.ApiKey);
+            Thread.Sleep(1000);
+            var allStations = await Stations.GetAllStationsAsync();
+            Thread.Sleep(1000);
+            foreach (var var1 in allStations)
+            {
+                Console.WriteLine(var1.Name);
+            }
+
+            Console.WriteLine("Finished");
+            Console.ReadKey();
         }
     }
 }
