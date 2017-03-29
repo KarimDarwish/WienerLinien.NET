78560dbb42194ae90b66a07c9489457c5991f4ad
Johannes <johannes_mayerl@hotmail.com>
2017-03-29 15:50:26 +0200

    Started to migrate UI to be reworked from the ground up added clock

---
+++ WienerLinienApi.Samples.WPF_Proper/Model/Clockconverter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WienerLinienApi.Samples.WPF_Proper.Model
+{
+    public class ClockConverter : IValueConverter
+    {
+        #region IValueConverter Member
+
+        public object  Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+ 	        String zeigertyp = parameter as String;
+            if (zeigertyp == null) zeigertyp = "Sekunde";
+            
+
+            if (value is DateTime)
+            {
+                var zeit = (DateTime) value;
+                switch (zeigertyp.Substring(0,1).ToLower())
+                {
+                    case "h" :
+                        return (zeit.Hour * 30) + (zeit.Minute * 0.5)+ (zeit.Second * (1 / 120));
+                    case "m" :
+                        return zeit.Minute * 6;
+                    default :
+                        return zeit.Second * 6;
+                }                
+            }
+            else return 0;
+        }
+
+        public object  ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+ 	        throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
---
+++ WienerLinienApi.Samples.WPF_Proper/Model/NotifyingDateTime.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Threading;
+using System.ComponentModel;
+
+namespace WienerLinienApi.Samples.WPF_Proper.Model
+{
+   public class NotifyingDateTime : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        private DateTime _now;
+
+        public NotifyingDateTime()
+        {
+            _now = DateTime.Now;
+            DispatcherTimer timer = new DispatcherTimer();  
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += timer_Tick; 
+            timer.Start();
+        }
+
+        public DateTime Now
+        {
+            get { return _now; }
+            private set
+            {                      
+                _now = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs("Now"));
+            }
+        }
+
+        void timer_Tick(object sender, EventArgs e)
+        {
+            Now = DateTime.Now; 
+        }
+
+    }
+}
