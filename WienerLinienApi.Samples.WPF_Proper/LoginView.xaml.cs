88d06d0668e6e1f213fbe92f522519ff1f963cae
Johannes <johannes_mayerl@hotmail.com>
2017-04-30 22:42:56 +0200

    Login Page is reaady for back-end

---
+++ WienerLinienApi.Samples.WPF_Proper/LoginView.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WienerLinienApi.Samples.WPF_Proper
+{
+    /// <summary>
+    /// Interaction logic for LoginView.xaml
+    /// </summary>
+    public partial class LoginView : UserControl
+    {
+        public bool isLogin { get; set; }
+        public LoginView()
+        {
+            isLogin = true;
+            InitializeComponent();
+            Storyboard sb = (this.FindResource("LoginPrep") as Storyboard);
+            sb.Begin();
+            
+        }
+
+        private void LoginPrep_Storyboard_Completed(object sender, EventArgs e)
+        {
+            Storyboard sb = (this.FindResource("LoginApear") as Storyboard);
+            sb.Begin();
+        }
+
+        private void SignUpPrep_Storyboard_Completed(object sender, EventArgs e)
+        {
+            Storyboard sb = (this.FindResource("SignUpAnimation") as Storyboard);
+            sb.Begin();
+        }
+        
+        private void SignUp_Click(object sender, RoutedEventArgs e)
+        {
+            if (isLogin) {
+                ToggleMenu();
+            }
+        }
+
+        private void Login_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isLogin)
+            {
+                ToggleMenu();
+            }
+        }
+
+        private void ToggleMenu() {
+            if (isLogin)
+            {
+                Storyboard sb = (this.FindResource("SignUpPrep") as Storyboard);
+                sb.Begin();
+
+            }
+            else {
+                Storyboard sb = (this.FindResource("LoginPrep") as Storyboard);
+                sb.Begin();
+            }
+
+            isLogin = !isLogin;
+        }
+       
+    }
+}
--- WienerLinienApi.Samples.WPF_Proper/MainWindow.xaml.cs
+++ WienerLinienApi.Samples.WPF_Proper/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +22,26 @@ namespace WienerLinienApi.Samples.WPF_Proper
     /// </summary>
     public partial class MainWindow : Window
     {
+        public Object MainContent { get; set; }
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainContent = Content;
+            Storyboard sb = (this.FindResource("LoginAnimation") as Storyboard);
+            sb.Begin();
+            
+        }
+
+        private void Login_Storyboard_Completed(object sender, EventArgs e)
+        {
+            Content = null;
+            Content = new LoginView();
+            
         }
     }
 }
